623215d1a389af91b9508d616ffc1ab7ef9427c6
Simon Buyck <simon.buyck@student.howest.be>
2025-11-28 09:26:39 +0100

    fix routes

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -61,7 +61,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_APP_BASE_URL),
+  history: createWebHistory(),
   routes,
   scrollBehavior: async (_to, _from, _savedPosition) => {
     return { left: 0, top: 0, behavior: "instant" };
